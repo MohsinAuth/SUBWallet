08075a4c14ad990135a1e104457d64da1bd20e0d
MohsinAuth <mohsin@authornate.com>
2022-04-14 11:26:50 +0500

    Update

--- index.ts
+++ index.ts
@@ -1,38 +1,10 @@
-import {
-    MnemonicKey,
-    LCDClient,
-    MsgExecuteContract,
-} from "@terra-money/terra.js";
+import { MnemonicKey } from "@terra-money/terra.js";
 import express from "express";
 import { MongoClient } from "mongodb";
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// const terra = new LCDClient({
-//     URL: "https://bombay-lcd.terra.dev/",
-//     chainID: "bombay-12",
-// });
-
-// const wallet = terra.wallet(MNE_KEY_RANDOM);
-
-// const execute = new MsgExecuteContract(
-//     wallet.key.accAddress, // sender
-//     "terra1ec4vxhuh8jpmqsu4ermsc9zvt6mx3z0pqtdktj", // contract account address
-//     { create_account: {} }, // handle msg
-//     { uusd: 200000 } // coins
-// );
-
-// async function create() {
-//     const executeTx = await wallet.createAndSignTx({
-//         msgs: [execute],
-//     });
-
-//     const executeTxResult = await terra.tx.broadcast(executeTx);
-//     console.log(executeTxResult);
-// }
-// create();
 
 app.get("/createWallet", async (_req, res) => {
     const client = new MongoClient(
